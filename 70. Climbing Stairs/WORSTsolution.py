2f99c74a85d46927d5b37e3397415672259d9dfd
Ari <91539966+deCafLatte@users.noreply.github.com>
2023-05-30 21:15:59 +0800

    Create WORSTsolution.py

---
+++ 70. Climbing Stairs/WORSTsolution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def climbStairs(self, n):
+        def climb(n):
+            if n==1: 
+                return 1
+            if n ==2: 
+                return 2
+            return climb(n-1) + climb(n-2)
+        return climb(n)
+    
